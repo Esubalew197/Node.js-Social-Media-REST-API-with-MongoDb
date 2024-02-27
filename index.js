bb8588b2b14b5a58b3732a0c7d5ee95be32263c7
Esubalew197 <Esubalewmillion@gmail.com>
2024-02-27 20:16:44 +0300

    create  MongoDB connection

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+dotenv.config();
 
+async function connectToDatabase() {
+    try {
+      await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+    }
+  }
+  
+  connectToDatabase();
 
 
 app.listen(8800, () => {
