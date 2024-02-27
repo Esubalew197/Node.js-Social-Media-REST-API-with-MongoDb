564e5b33d88284dad2162291fc7bb6b50d6c494d
Esubalew197 <Esubalewmillion@gmail.com>
2024-02-27 20:38:57 +0300

    creating user and auth routes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -25,6 +27,9 @@ async function connectToDatabase() {
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 
 app.listen(8800, () => {
